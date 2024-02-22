6bfd7eef3df8933f93c8524725b49a3bafd69b32
Matoss892 <157516620+Matoss892@users.noreply.github.com>
2024-02-22 17:30:38 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -47,7 +47,7 @@ $(function () {
 createPlatform (125,900,200,10); 
 createPlatform (1000,1000, 300,15);
 createPlatform (900,900,400,25);
-createPlatform (700,700,350,15);
+createPlatform (1100,700,350,15);
 createPlatform (800,800, 250,15);
     
     // TODO 2
